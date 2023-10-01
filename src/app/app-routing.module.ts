267ca1b7110bbe05e9de0fa649c7994cf65c08ef
rajat <fzdsharmarajat@gmail.com>
2023-10-01 20:14:07 +0530

    done1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  component :AboutComponent,
+  path:'about'
+},
+
+{
+   component :HomeComponent,
+   path:''
+},
+
+{
+   component: UserComponent,
+   path:'user/:id'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
